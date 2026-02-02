b6ac561a3fc36cf932f094852517c71c94486ce7
Capung01d <168819229+Capung01d@users.noreply.github.com>
2026-02-02 19:48:50 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyber: {
+          black: "#050505",
+          dark: "#0a0a0a",
+          red: "#ff0000",
+          blood: "#8b0000",
+        },
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
   },
   plugins: [],
 }
